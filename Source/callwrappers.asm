dc75c597d065c5a39925cd7bc09e1d1838b5984f
Dalton Barreto <daltonmatos@gmail.com>
2015-11-05 18:30:46 -0200

    Assembly main entrypoints can be called directly

--- Source/callwrappers.asm
+++ Source/callwrappers.asm
@@ -2,18 +2,3 @@
 
 .include "bindwrappers.asm"
 
-safe_Main:
-  safe_called_from_c Main
-  ret
-
-safe_CppmMain:
-  safe_called_from_c CppmMain
-  ret
-
-safe_SBusMain:
-  safe_called_from_c SBusMain
-  ret
-
-safe_SatelliteMain:
-  safe_called_from_c SatelliteMain
-  ret
