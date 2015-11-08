ef53943ca194c056418568ad7109b562c96219fa
Dalton Barreto <daltonmatos@gmail.com>
2015-11-08 18:43:14 -0200

    Much cleaner RAM variable declaration

--- Source/ramvariables.h
+++ Source/ramvariables.h
@@ -2,24 +2,15 @@
 #define RAM_VARIABLES_H
 
 #define uint8_t_prt(a) ((uint8_t *) (a))
+#define RAM_VARIABLE(addr) (*(uint8_t_prt(addr)))
 
-#define __XPos 0x073D
-#define XPos (*(uint8_t_prt(__XPos)))
+#define XPos RAM_VARIABLE(0x073D)
+#define YPos RAM_VARIABLE(0x073E)
+#define X1 RAM_VARIABLE(0x073F)
+#define Y1 RAM_VARIABLE(0x0740)
 
-#define __YPos 0x073E
-#define YPos (*(uint8_t_prt(__YPos)))
-
-#define __PixelType 0x0743
-#define PixelType (*(uint8_t_prt(__PixelType)))
-
-#define __X1 0x073F
-#define X1 (*(uint8_t_prt(__X1)))
-
-#define __Y1 0x0740
-#define Y1 (*(uint8_t_prt(__Y1)))
-
-#define __FontSelector 0x0744
-#define FontSelector (*(uint8_t_prt(__FontSelector)))
+#define PixelType RAM_VARIABLE(0x0743)
+#define FontSelector RAM_VARIABLE(0x0744)
 
 #define RxMode 0x074D
 #define UserProfile 0x0803
