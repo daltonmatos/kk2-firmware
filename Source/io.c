e550ad3b2654ccb4be9f91673ceb0f46efae2718
Dalton Barreto <daltonmatos@gmail.com>
2015-11-23 14:49:42 -0200

    New print_string that can highlight printed text

--- Source/io.c
+++ Source/io.c
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <string.h>
+
 #include "io.h"
 #include "ramvariables.h"
 #include "display/st7565.h"
@@ -41,13 +43,16 @@ uint8_t __get_buttons_blocking(){
 }
 
 
-void print_string(const uint8_t *str_addr, uint8_t x, uint8_t y){
+uint8_t print_string(const uint8_t *str_addr, uint8_t x, uint8_t y){
   X1 = x;
   Y1 = y;
   char ch = 0;
+  uint8_t count = 0;
   while ((ch = pgm_read_byte(str_addr++))){
     asm_PrintChar(ch);
+    count++;
   }
+  return count;
 }
 
 void print_number(int8_t number, uint8_t x, uint8_t y){
@@ -66,6 +71,8 @@ extern const char confirm;
 extern const char conf;
 extern const char rusure;
 
+/* This holds info about each font. We will need CharWidth and CharHeight */
+extern const char TabCh;
 
 uint8_t show_confirmation_dlg(const uint8_t *str){
   
@@ -89,3 +96,33 @@ uint8_t show_confirmation_dlg(const uint8_t *str){
 
 }
 
+void print_selector(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2){
+  X1 = x1;
+  X2 = x2;
+  Y1 = y1;
+  Y2 = y2;
+  PixelType = 0;
+  asm_HighlightRectangle();
+}
+
+void print_string_2(const uint8_t *str_addr, uint8_t x, uint8_t y, uint8_t hilight_type){
+  uint8_t len = print_string(str_addr, x, y);
+  uint8_t char_width = pgm_read_byte(&TabCh + (FontSelector * 6) + 2);
+  uint8_t char_height = pgm_read_byte(&TabCh + (FontSelector * 6) + 3);
+
+  switch (hilight_type){
+    case HIGHLIGHT_STRING:
+      print_selector(x, y-1, x + (len * char_width), y + char_height);
+      break;
+    case HIGHLIGHT_FULL_LINE:
+      print_selector(0, y-1, 127, y + char_height);
+      break;
+    case HIGHLIGHT_TO_THE_END_OF_LINE:
+      print_selector(x, y-1, 127, y + char_height);
+      break;
+    case HIGHLIGHT_FROM_BEGINNING_OF_LINE:
+      print_selector(0, y-1, x + (len * char_width), y + char_height);
+      break;
+  }
+
+}
--- Source/io.h
+++ Source/io.h
@@ -7,6 +7,14 @@
 #define BUTTON_BACK 0x08
 #define BUTTON_ANY BUTTON_OK | BUTTON_DOWN | BUTTON_UP | BUTTON_BACK
 
+enum {
+  HIGHLIGHT_NONE = 1,
+  HIGHLIGHT_STRING,
+  HIGHLIGHT_FULL_LINE,
+  HIGHLIGHT_TO_THE_END_OF_LINE,
+  HIGHLIGHT_FROM_BEGINNING_OF_LINE
+};
+
 extern void asm_PrintChar(const char ch);
 extern uint8_t asm_GetButtonsBlocking();
 extern void asm_ShowNoAccessDlg(uint8_t *str);
@@ -14,9 +22,13 @@ extern void asm_Print16Signed(int8_t number);
 extern void asm_HighlightRectangle();
 extern void asm_EnforceRestart();
 
-void print_string(const uint8_t *str_addr, uint8_t x, uint8_t y);
+uint8_t print_string(const uint8_t *str_addr, uint8_t x, uint8_t y);
+void print_string_2(const uint8_t *str_addr, uint8_t x, uint8_t y, uint8_t hilight);
 void print_number(int8_t number, uint8_t x, uint8_t y);
 uint8_t wait_for_button(uint8_t button_mask);
 uint8_t show_confirmation_dlg(const uint8_t *str);
+void print_selector(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+
+
 
 #endif
