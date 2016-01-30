69aee39c5cc4a0229da45c39158674226d2d0d89
Dalton Barreto <daltonmatos@gmail.com>
2016-01-29 23:26:16 -0200

    First implementation for the 16.8 fixed-point math

--- Source/b168.c
+++ Source/b168.c
@@ -2,11 +2,25 @@
 #include "b168.h"
 #include "io.h"
 
+int32_t b168_pack(b168_t *v){
+  int32_t _v = v->hi;
+  _v <<= 16;
+  _v |= (v->lo << 8);
+  _v |= v->decimal;
+  return _v;
+}
 
-uint8_t __print8_decimal(b168_t *number, uint8_t precision){
+b168_t *b168_unpack(int32_t _v, b168_t *dest){
+  dest->hi = (_v >> 16);
+  dest->lo = (_v >> 8);
+  dest->decimal = _v;
+  return dest;
+}
 
-  uint16_t frac = number->decimal;
+uint8_t __print8_decimal(uint16_t frac, uint8_t precision){
+
   uint16_t digits = 0;
+  
   while (frac && precision){
     frac &= 0x00ff;
     frac *= 10;
@@ -17,14 +31,49 @@ uint8_t __print8_decimal(b168_t *number, uint8_t precision){
   return digits;
 }
 
-uint8_t print_b168(b168_t *number, uint8_t precision){
+uint8_t b168_print(b168_t *number, uint8_t precision, uint8_t x, uint8_t y){
   uint8_t digits = 0;
-  digits = _print16_signed((number->hi << 8) | number->lo);
+  int32_t packed = b168_pack(number);
+
+  X1 = x;
+  Y1 = y;
+  if (packed < 0){
+    print_char('-');
+    packed = -packed;
+  }
+
+  digits = _print16_signed(packed >> 8);
   print_char('.');
-  digits += __print8_decimal(number, precision);
+  digits += __print8_decimal(packed, precision);
   if (!number->decimal){
     _print16_signed(0);
   }
   return ++digits;
 }
 
+b168_t *b168_neg(b168_t *v){
+  b168_unpack(-b168_pack(v), v);
+}
+
+void b168clr(b168_t *number){
+  number->hi = 0;
+  number->lo = 0;
+  number->decimal = 0;
+}
+
+void b168set(b168_t *n, uint8_t c){
+  n->hi = c;
+  n->lo = c;
+  n->decimal = c;
+}
+
+void b168mov(b168_t *dest, b168_t *src){
+  dest->hi = src->hi;
+  dest->lo = src->lo;
+  dest->decimal = src->decimal;
+}
+
+
+void b168nmov(b168_t *dest, b168_t *src){
+  b168mov(dest, b168_neg(src));
+}
--- Source/b168.h
+++ Source/b168.h
@@ -5,14 +5,48 @@
 
 typedef struct {
   int8_t hi;
-  uint8_t lo;
+  int8_t lo;
   uint8_t decimal;
 } b168_t;
 
-#define ENCODE_TO_8BIT_PRECISION(value) ((value) / (1.0/(256.0)))
+#define PRECISION 0.00390625
+#define ENCODE3(value) ((int32_t) ((value) / (PRECISION)))
+#define b168_load(b168_prt, value) b168_unpack(ENCODE3(value), b168_prt)
 
-uint8_t print_b168(b168_t *number, uint8_t precision);
+#define ENCODE_TO_8BIT_PRECISION(value) ((uint8_t) ((value) / (PRECISION)))
 
-void b168_const_add(b168_t *a, int16_t value);
+int32_t b168_pack(b168_t *v);
+b168_t *b168_unpack(int32_t _v, b168_t *dest);
+
+uint8_t b168_print(b168_t *number, uint8_t precision, uint8_t x, uint8_t y);
+
+void b168fadd(b168_t *a, int16_t value);
+
+b168_t *b168_neg(b168_t *v);
+void b168clr(b168_t *v);
+void b168set(b168_t *n, uint8_t c);
+void b168mov(b168_t *dest, b168_t *src); 
+void b168nmov(b168_t *dest, b168_t *src); /* Negate src and move to dest */
+
+/* Implemented in Assembly*/
+/* _add and _mul must receive 3 parameters. Signature must be: (b168_t *dest, b168_t *a, b168_t *b)*/
+void b168_add(b168_t *a, b168_t *b); /* a + b, stores result inside a */
+void b168_mul(b168_t *a, b168_t *b); /* a * b, stores result inside a */
+
+void b168fmul(b168_t *number, uint8_t value); /* multiply number by 2^value */
+void b168fdiv(b168_t *number, uint8_t value); /* divide number by 2^value */
+
+/* TODO: Implement */
+void b168sub(b168_t *dest, b168_t *a, b168_t *b); /* dest = a -b */
+void b168dec(b168_t *number);
+void b168inc(b168_t *number);
+void b168mov2(b168_t *dest1, b168_t *dest2, b168_t *src); /*moves src to dest1 and dest2 . Will be implemented in C*/
+void b168cmp(b168_t *a, b168_t *b);
+void b168mac(b168_t *acc, b168_t *n); /* acc = acc * n */
+
+void b168load(b168_t *number); /* xh:xl.yh <- number */
+void b168store(b168_t *number); /* number <- xh:xl.yh */
+
+void b168loadz(b168_t *number); /* Z = int(number) */
 
 #endif
